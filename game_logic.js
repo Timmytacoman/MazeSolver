1d0f1cc6791b63ba3f5cd69e389571333227d5ab
Foreman <e0ti3061@nwe.northwesternenergy.com>
2023-08-08 15:58:47 -0600

    add button click incrementer

--- game_logic.js
+++ game_logic.js
@@ -11,15 +11,25 @@ function drawRectangle(lineWidth, strokeStyle, left, top, width, height) {
   ctx.strokeStyle = strokeStyle;
   ctx.rect(left, top, width, height);
   ctx.stroke();
-
 }
 
 function buildGrid() {
-  drawRectangle("6", "red", 5, 5, 290, 140);
+  //   drawRectangle("6", "red", 5, 5, 290, 140);
+  for (var i = 0; i < 1000; i++) {
+    console.log(i);
+  }
 }
 
 function test() {
-  let tbl = document.getElementById("pixelCanvas");
-
-  console.log(tbl);
+  console.log("test");
+  //   let tbl = document.getElementById("pixelCanvas");
+  //   tbl.remove();
 }
+
+
+var i = 0;
+var button = document.getElementById("goButton");
+button.addEventListener("click", function () {
+  i++;
+  goButton.innerHTML = i;
+});
