86009796d425fa0589bf7fc0b32b141a7cdb2482
Foreman <e0ti3061@nwe.northwesternenergy.com>
2023-08-08 15:46:59 -0600

    add drawRectangle function

--- game_logic.js
+++ game_logic.js
@@ -1,6 +1,24 @@
-function test() {
-  console.log("here");
+let w = 10;
+let h = 10;
 
+function drawRectangle(lineWidth, strokeStyle, left, top, width, height) {
+  console.log("drawing rect");
+  const canvas = document.getElementById("pixelCanvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = strokeStyle;
+  ctx.rect(left, top, width, height);
+  ctx.stroke();
+
+}
+
+function buildGrid() {
+  drawRectangle("6", "red", 5, 5, 290, 140);
+}
+
+function test() {
   let tbl = document.getElementById("pixelCanvas");
 
   console.log(tbl);
