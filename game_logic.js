885445726b078a199e6593e5b717f0142fe6ffc4
Foreman <e0ti3061@nwe.northwesternenergy.com>
2023-08-09 11:10:13 -0600

    start interactive grid

--- game_logic.js
+++ game_logic.js
@@ -1,35 +1,41 @@
-let w = 10;
-let h = 10;
+function tableCreate() {
+  const body = document.body;
+  const tbl = document.createElement("table");
 
-function drawRectangle(lineWidth, strokeStyle, left, top, width, height) {
-  console.log("drawing rect");
-  const canvas = document.getElementById("pixelCanvas");
-  const ctx = canvas.getContext("2d");
-
-  ctx.beginPath();
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = strokeStyle;
-  ctx.rect(left, top, width, height);
-  ctx.stroke();
+  for (let i = 0; i < 10; i++) {
+    const tr = tbl.insertRow();
+    for (let j = 0; j < 10; j++) {
+      const td = tr.insertCell();
+      td.id = i + "," + j;
+      td.addEventListener("mouseenter", clickMethod);
+      // td.addEventListener("click", (event) => {
+        // td.textContent = `Click count: ${event.detail}`;
+        // console.log(event.altKey)
+        // if (event.altKey) {
+        //   td.style.backgroundColor = "black";
+        // }
+        // else {
+        //   td.style.backgroundColor = "#96D4D4"
+        // }
+      // });
+    }
+  }
+  body.appendChild(tbl);
 }
 
-function buildGrid() {
-  //   drawRectangle("6", "red", 5, 5, 290, 140);
-  for (var i = 0; i < 1000; i++) {
-    console.log(i);
+
+function clickMethod(event) {
+  console.log(event);
+  if(event.buttons == 1) {
+    // console.log("left click detected!")
+    // console.log(event.srcElement)
+    event.srcElement.style.backgroundColor = "black";
+  }
+
+  else if (event.altKey) {
+    event.srcElement.style.backgroundColor = "#96D4D4";
   }
 }
 
-function test() {
-  console.log("test");
-  //   let tbl = document.getElementById("pixelCanvas");
-  //   tbl.remove();
-}
 
-
-var i = 0;
-var button = document.getElementById("goButton");
-button.addEventListener("click", function () {
-  i++;
-  goButton.innerHTML = i;
-});
+tableCreate();
